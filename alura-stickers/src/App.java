b46263c1362b645cdb58a129a619364cca88d594
Jaqueline Bravin <111389555+JaquelineBravin@users.noreply.github.com>
2023-03-27 12:03:25 -0400

    primeiros passos

---
+++ alura-stickers/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        //fazer a conexão HTTP E BUSCAR OS TOP 250 FILMES
+        String url = "";
+        Httpc
+
+        // pegar só os dados interessantes (titulo, poster, classificação)
+        // exibir e manipular 
+    }
+}
