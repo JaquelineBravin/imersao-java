5dc80c8eca60bbe62c25d0bcd1038d490e2d46b9
Jaqueline Bravin <111389555+JaquelineBravin@users.noreply.github.com>
2023-03-27 18:00:58 -0400

    criando imports

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,4 +1,6 @@
 import java.net.URI;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -9,9 +11,13 @@ public class App {
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient;
         var request = HttpRequest.newBuilder(endereco).GET().build();
-        client.send(request, BodyHandlers)
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
 
         // pegar só os dados interessantes (titulo, poster, classificação)
+        JsonParser jsonParser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = body;
         // exibir e manipular 
     }
 }
