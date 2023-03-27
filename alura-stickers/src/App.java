4542ea063fa7b5e5be63ab7796fbcc317684cf36
Jaqueline Bravin <111389555+JaquelineBravin@users.noreply.github.com>
2023-03-27 14:52:18 -0400

    Update App.java

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,10 +1,15 @@
+import java.net.URI;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
         //fazer a conexão HTTP E BUSCAR OS TOP 250 FILMES
-        String url = "";
-        Httpc
+        String url = "gfdgfgfdgdfgddg";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient;
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        client.send(request, BodyHandlers)
 
         // pegar só os dados interessantes (titulo, poster, classificação)
         // exibir e manipular 
