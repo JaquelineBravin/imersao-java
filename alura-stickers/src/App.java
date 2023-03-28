1677b737cbaddcf41617cded595e22b7e844f001
JaquelineBravin <111389555+JaquelineBravin@users.noreply.github.com>
2023-03-27 23:54:34 -0400

    adicionando filmes

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -3,6 +3,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -11,20 +12,23 @@ public class App {
         System.out.println("Hello, World!");
 
         //fazer a conexão HTTP E BUSCAR OS TOP 250 FILMES
-        String url = "gfdgfgfdgdfgddg";
-//        String url = "imbd-api.com";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
-//        Httpclient
 
         // pegar só os dados interessantes (titulo, poster, classificação)
-//        JsonParser jsonParser = new JsonParser();
-//        List<Map<String, String>> listaDeFilmes = body;
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        System.out.println(listaDeFilmes.size());
+        
         // exibir e manipular 
-        System.out.println(body);
+        for (Map<String, String> filme : listaDeFilmes) {
+        	System.out.println(filme.get("title"));
+        	System.out.println(filme.get("image"));
+        	System.out.println(filme.get("imDbRating"));
+        }
     }
 }
